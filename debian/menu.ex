bdc11329c78b343daad86420312159a5cceab655
Devon Kearns <dookie@kali.org>
2012-12-29 08:35:24 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sakis3g):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="sakis3g" command="/usr/bin/sakis3g"
--- debian/sakis3g.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sakis3g package
-#
-0 4	* * *	root	[ -x /usr/bin/sakis3g_maintenance ] && /usr/bin/sakis3g_maintenance
